071a5531571f01dc3c9abe502f19b0fd68fd028b
davidinmichael <davidinmichael@gmail.com>
2024-12-02 12:27:24 +0100

    MD's desk added

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import (
-	Home, CorporateView,
+	Home, CorporateView, MDDesk
 )
 
 
 urlpatterns = [
 	path("", Home.as_view(), name="home"),
 	path("corporate-overview/", CorporateView.as_view(), name="corporate"),
+	path("mddesk/", MDDesk.as_view(), name="mddesk"),
 ]
--- core/views.py
+++ core/views.py
@@ -11,3 +11,12 @@ class Home(View):
 class CorporateView(View):
 	def get(self, request):
 		return render(request, "core/corporate_overview.html")
+	
+
+class MDDesk(View):
+	def get(self, request):
+		return render(request, "core/mddesk.html")
+
+class MissionVision(View):
+	def get(self, request):
+		return render(request, "core/mission.html")
